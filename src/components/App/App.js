c29c5944b22dcbad1afa996e4475a7597dade005
Anne Culpepper <anne.culpepper@gmail.com>
2020-07-30 18:48:53 -0500

    Built out an Admin page and registered it as a route in App component. Admin page has Login, Logout, AddGenre, GenresList, and GenresListItem components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import HomePage from '../Pages/HomePage/HomePage';
 import DetailsPage from '../Pages/DetailsPage/DetailsPage';
 import EditPage from '../Pages/EditPage/EditPage';
+import AdminPage from '../Pages/AdminPage/AdminPage';
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -16,6 +17,7 @@ class App extends Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/details/:id' component={DetailsPage} />
           <Route path='/edit/:id' component={EditPage} />
+          <Route path='admin' component={AdminPage} />
         </Router>
       </div>
     );
---
+++ src/components/GenresListItem/GenresListItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class GenresListItem extends Component {
+  clickRemove = (event) => {
+    this.props.dispatch({
+      type: 'DELETE_GENRE',
+      payload: {
+        genreId: this.props.item.id,
+      },
+    });
+  };
+
+  render() {
+    const { item } = this.props;
+
+    return (
+      <li>
+        {item.name} <button onClick={this.clickRemove}>X</button>
+      </li>
+    );
+  }
+}
+
+const mapStoreToProps = (store) => ({ store });
+
+export default connect(mapStoreToProps)(GenresListItem);
