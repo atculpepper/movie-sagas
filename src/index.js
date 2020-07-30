902ad965db5f91cb8358de245d26e14c98fa0d6c
Anne Culpepper <anne.culpepper@gmail.com>
2020-07-30 18:14:35 -0500

    editing getGenres sata to a different file path from server

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ function* fetchDetails(action) {
 function* getGenres(action) {
   try {
     const movieId = action.payload;
-    const response = yield axios.get(`api/movies/genres/${movieId}`);
+    const response = yield axios.get('api/genres');
     yield put({
       type: 'SET_GENRES',
       payload: response.data,
