bedf4227956074fd3f934b281110adfb8696f841
Anne Culpepper <anne.culpepper@gmail.com>
2020-07-30 18:22:18 -0500

    registered auth route and router in server file

--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const moviesRouter = require("./routes/movies.router");
-const genresRouter = require("./routes/genres.router");
+const moviesRouter = require('./routes/movies.router');
+const genresRouter = require('./routes/genres.router');
+const authRouter = require('./routers/auth.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
-app.use(express.static("build")); //serving up static files
+app.use(express.static('build')); //serving up static files
 app.use(bodyParser.urlencoded({ extended: true })); //tuck sent data onto req.body
 
 /** ---------- ROUTES ---------- **/
 //Register moviesRouter
-app.use("/api/movies", moviesRouter);
-app.use("/api/genres", genresRouter);
+app.use('/api/movies', moviesRouter);
+app.use('/api/genres', genresRouter);
+// create / register new login route
+app.use('/api/auth', authRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
-  console.log("Server is running on port: ", port);
+  console.log('Server is running on port: ', port);
 });
