9814df2c64336589d00219e3b8bccabbf9929b3d
Anne Culpepper <anne.culpepper@gmail.com>
2020-08-04 18:38:33 -0500

    no longer using class App on App.js

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -3,7 +3,11 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import MoviesListItem from '../MoviesListItem/MoviesListItem';
 
+//custom components
 import Header from '../Header/Header';
+
+//material ui components
+import Container from '@material-ui/core/Container';
 
 class MoviesList extends Component {
   // state = {
@@ -43,12 +47,12 @@ class MoviesList extends Component {
     });
 
     return (
-      <div className='algnLeft'>
+      <Container className='algnLeft'>
         {/* <input placeholder='Search' type='text' onChange={this.changeSearch} /> */}
         {limitedResults.map((item, index) => (
           <MoviesListItem key={index} item={item} />
         ))}
-      </div>
+      </Container>
     );
   }
 }
